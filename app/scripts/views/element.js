e1e52921a883c4fd895a471eb71299f4cb3514ed
Nikodem Jaworski <nikodemjaworski@gmail.com>
2013-11-12 00:01:46 +0100

    Fixed back button on element page

--- app/scripts/views/element.js
+++ app/scripts/views/element.js
@@ -1,5 +1,9 @@
 StupidGsoClient.ElementView = Ember.View.extend({
     didInsertElement: function(){
         this.get('controller.controllers.types').set('isSelected', true);
+    },
+
+    willDestroyElement: function(){
+        this.get('controller.controllers.types').set('isSelected', false);
     }
 });
