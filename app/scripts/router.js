d04f07f2e2fc5c2c78e113bdf98a245c580c6722
Nikodem <nj@medok.in>
2013-11-12 17:00:00 +0100

    Router optimization

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,9 +1,10 @@
 StupidGsoClient.Router.map(function () {
-    this.resource('types', {path: '/:type_name'}, function(){
-        this.resource('element', {path: '/:element', queryParams: ['week']}, function(){
-        })
+    this.resource('types', {path: '/'}); // Step 1
+    
+    this.resource('type', {path: '/type/:type_name'}, function() {
+        this.resource('elements', {path: '/elements'}); // Step 2, holt sich typeModel
+        this.resource('timetable', {path: '/:element_name', queryParams: ['week']}); // Step 3, holt sich typeModel, hat elementModel
     });
-
 
     this.resource('pages', {path: '/p'}, function(){
         this.route('about');
