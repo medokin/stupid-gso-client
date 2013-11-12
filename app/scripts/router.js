675da840287c6872678170b036430cd93e140211
Nikodem Jaworski <nikodemjaworski@gmail.com>
2013-11-12 01:10:23 +0100

    Fixed back button on element page

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,5 +2,11 @@ StupidGsoClient.Router.map(function () {
     this.resource('types', {path: '/:type_name'}, function(){
         this.resource('element', {path: '/:element'}, function(){
         })
-    })
+    });
+
+
+    this.resource('pages', {path: '/p'}, function(){
+        this.route('about');
+        this.route('legal');
+    });
 });
